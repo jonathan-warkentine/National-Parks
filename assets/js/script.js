e537248bd9dde8205894418d5104f33ffd7a5b2e
amachkel <amachkel@gmail.com>
2022-02-25 14:32:11 -0500

    searchBtn searches on map for state. State input is case insensitive.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
-let longitude = -97;
-let latitude = 38;
+function renderMap(longitude, latitude, zoom) {
+  require(["esri/config", "esri/Map", "esri/views/MapView"], function (
+    esriConfig,
+    Map,
+    MapView
+  ) {
+    esriConfig.apiKey =
+      "AAPK69742b5d3e5d4d969f28ce8b97ee91f9c-GZhvscrk59aNtlQqY1LEIYm6FP_SH-3eVXanS5UfS9755ehIGeGrMn0_NmE_pP";
 
-require(["esri/config", "esri/Map", "esri/views/MapView"], function (
-  esriConfig,
-  Map,
-  MapView
-) {
-  esriConfig.apiKey =
-    "AAPK69742b5d3e5d4d969f28ce8b97ee91f9c-GZhvscrk59aNtlQqY1LEIYm6FP_SH-3eVXanS5UfS9755ehIGeGrMn0_NmE_pP";
+    const map = new Map({
+      basemap: "arcgis-topographic", // Basemap layer service
+    });
 
-  const map = new Map({
-    basemap: "arcgis-topographic", // Basemap layer service
+    const view = new MapView({
+      map: map,
+      center: [longitude, latitude], // Longitude, latitude
+      zoom: zoom, // Zoom level
+      container: "mapDiv", // Div element
+    });
   });
-
-  const view = new MapView({
-    map: map,
-    center: [longitude, latitude], // Longitude, latitude
-    zoom: 4, // Zoom level
-    container: "mapDiv", // Div element
-  });
-});
-
+}
 
 $("#searchBtn").click(function () {
-    let searchEl = $("#select-state").val();
-    console.log(searchEl);
-    for (let i = 0; i < statesArray.length; i++) {
-        if (searchEl === statesArray[i].state) {
-            console.log(statesArray[i].latitude);
-            console.log(statesArray[i].longitude);
-        }
+  let searchEl = $("#select-state").val().toLowerCase();
+  console.log(searchEl);
+  for (let i = 0; i < statesArray.length; i++) {
+    if (searchEl === statesArray[i].state.toLocaleLowerCase()) {
+      console.log(statesArray[i].latitude);
+      console.log(statesArray[i].longitude);
+      renderMap(statesArray[i].longitude, statesArray[i].latitude, 6);
     }
-  })
+  }
+});
+renderMap(-97, 38, 4);
